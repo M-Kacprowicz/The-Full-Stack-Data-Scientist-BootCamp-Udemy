7001dae73418e6d6ab1e03847623eb7914785b11
Mateusz <m.kacprowicz98@gmail.com>
2022-10-30 20:19:58 +0100

    SQL INTERMIDIATE LEVEL SECTION - SQL JOINS PART 2

--- SQL/SQL_INTERMEDIATE_LEVEL_SECTION_joins.sql
+++ SQL/SQL_INTERMEDIATE_LEVEL_SECTION_joins.sql
@@ -67,3 +67,52 @@ SELECT e.employee_id, e.first_name, e.last_name, m.first_name AS manager
 FROM employees e
 JOIN employees m
 	ON e.reports_to = m.employee_id;
+    
+-- JOINING ACROSS MULTIPLE TABLES
+USE sql_store;
+SELECT *
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+		JOIN order_statuses os
+			ON o.status = os.order_status_id;
+
+
+SELECT o.order_id, o.order_date, c.first_name, c.last_name, o.status, os.name AS status_meaning
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+		JOIN order_statuses os
+			ON o.status = os.order_status_id;
+            
+-- OUTER JOIN
+
+USE sql_store;
+
+SELECT c.customer_id, c.first_name, o.order_id
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id;
+    
+SELECT c.customer_id, c.first_name, o.order_id
+FROM customers c
+RIGHT JOIN orders o
+	ON c.customer_id = o.customer_id;
+    
+-- EXERCISE
+USE sql_store;
+SELECT p.product_id, p.name AS product_name, oi.quantity AS ordered_quantity
+FROM products p
+LEFT JOIN order_items oi
+	ON p.product_id = oi.product_id;
+    
+
+USE sql_invoicing;
+
+SELECT i.invoice_id, i.invoice_date, i.invoice_total, c.client_id, c.name AS client_name, 
+	pm.name AS payment_method
+FROM invoices i
+LEFT JOIN clients c USING(client_id)
+	LEFT JOIN payments p USING(invoice_id)
+		LEFT JOIN payment_methods pm
+			ON p.payment_method = pm.payment_method_id;
